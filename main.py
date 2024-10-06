be753ece32c9458eda0dcaddabe3a764357ac99f
nonoyjuswa <160596170+nonoyjuswa@users.noreply.github.com>
2024-10-06 14:50:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 pygame.init()
 
 window_width, window_height = 1280, 720
-display_screen = pygame.display.set_mode((windoow_width, window_height))
+display_screen = pygame.display.set_mode((window_width, window_height))
 running = True
 
 while running:
@@ -11,6 +11,6 @@ while running:
     if event.type == pygame.QUIT:
       running = False
 
-  display.fill('black')
+  display_screen.fill('black')
 
 pygame.quit()
